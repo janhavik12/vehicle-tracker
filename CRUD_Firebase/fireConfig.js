3a2e4b28d484ddb796d054ce2bbc49b495688e71
janhavik12 <109642595+janhavik12@users.noreply.github.com>
2023-05-06 20:18:19 +0530

    Add files via upload

---
+++ CRUD_Firebase/fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDUJeHLwpI-bHN67u9dfz3k2tbB1t6Ol7o",
+    authDomain: "sign-in-d1306.firebaseapp.com",
+    databaseURL: "https://sign-in-d1306-default-rtdb.firebaseio.com",
+    projectId: "sign-in-d1306",
+    storageBucket: "sign-in-d1306.appspot.com",
+    messagingSenderId: "1075476347941",
+    appId: "1:1075476347941:web:c35267323c007ecf1553cc",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
